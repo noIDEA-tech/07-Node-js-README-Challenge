51d633e146a269b1baf85fab937f619d0239e16d
Nancy Watreas <nwatreas2023@gmail.com>
2024-11-17 15:28:43 -0600

    added modified files

--- Backup/Develop/index.js
+++ Backup/Develop/index.js
@@ -1,30 +1,39 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
-import fs from 'fs';
-const generateMarkdown = require('./utils/generateMarkdown.js');
+import fs from 'fs/promises';
+import generateMarkdown from './utils/generateMarkdown.js';
 
-const licenses = ['None', 'MIT', 'Apache 2.0', 'BSD 3-Clause']
 // TODO: Create an array of questions for user input
 const questions = [ 
     {
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
+        validate: (input) => input.length > 0 ? true : 'Title is required!'
     },
     {
         type: 'input',
         name: 'description',
         message: 'Enter a description of your project:',
+        validate: (input) => input.length > 0 ? true : 'Description is required!'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for this project:',
+        choices: ['MIT', 'Apache 2.0', 'BSD 3-Clause', 'None']
     },
     {
         type: 'input',
         name: 'installation',
         message: 'Enter installation instructions:',
+        default: 'npm install'
     },
     {
         type: 'input',
         name: 'usage',
         message: 'Please enter information on how to use this project:',
+        validate: (input) => input.length > 0 ? true : 'Usage information required!'
     },
     {
         type: 'input',
@@ -35,12 +44,7 @@ const questions = [
         type: 'input',
         name: 'test',
         message: 'Please enter test instructions:',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Select a license for this project:',
-        choices: licenses
+        validate: (input) => input.length > 0 ? true : 'Test instructions required!'
     },
     {
         type: 'input',
@@ -55,21 +59,29 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(filename) {
-    const filename = "./dist/README.md";
-    
-    fs.writeFile(filename. data, function (err) {
-        err ? console.log(err) : console.log(filename + " successfully created!")
-    });  
-    
+async function writeToFile(fileName, data) {
+    try {
+        await fs.writeFile(fileName, data);
+        console.log('Succesfully created README.md!');
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
+ 
+async function init() {
+    try {
+
+        const answers = await inquirer.prompt(questions);
+
+        const markdownContent = generateMarkdown(answers);
+
+        await writeToFile('README.md', markdownContent);
+    } catch (err) {
+        console.error('Error initializing app:', err);
+    }
 }
 
-// TODO: Create a function to initialize app
-function init() {
-
-    inquirer.prompt(questions)
-    .then (answers => writeToFile(generateMarkdown(answers)))
-}
-
-// Function call to initialize app
 init();
+
+//MOVED COMMENTS TO LINE 89 TODO: Create a function to initialize app//
+//MOVED COMMENTS TO LINE 90//
--- Backup/Develop/utils/generateMarkdown.js
+++ Backup/Develop/utils/generateMarkdown.js
@@ -1,87 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let badge = "";
+    if (!license || license === 'None') return '';
 
-  if(license != "None") {
-    badge = "![License Badge](https://shields.io/badge/license-" + license + "-green)";
-  }
+    const badges = {
+      'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]',
+      'Apache 2.0': '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]',
+      'BSD 3-Clause': '[![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]'
+    };
 
-  return badge;
+    return badges[license] || '';
 }
 
 function renderLicenseLink(license) {
-  let licenseLink;
+    if (!license || license === 'None') return '';
+ 
+    const links = {
+      'MIT': '(https://opensource.org/licenses/MIT)',
+      'Apache 2.0': '(https://opensource.org/licenses/Apache-2.0)',
+      'BSD 3-Clause': '(https://opensource.org/licenses/BSD-3-Clause)'   
+    };
 
-    switch(license) {
-      case "MIT":
-        licenseLink = "https://mit-license.org/";
-        break;
-      case "Apache 2.0":
-        licenseLink =  "https://www.apache.org/licenses/LICENSE-2.0.html";
-        break;
-      case "BSD 3-Clause":
-        licenseLink =  "https://opensource.org/licenses/BSD-3-Clause";
-        break;
-
-      default:
-        licenseLink = "";
-        break;
-    }
-
-    return licenseLink;
+    return links[license] || '';
 }
 
 function renderLicenseSection(license) {
-  let licenseSect = "";
+    if (!license || license === 'None') return '';
+  
+    return `## License
+    This project is licensed under the ${license} license. ${renderLicenseBadge(license)}${renderLicenseLink(license)}`;
 
-  if (license != "None") {
-    licenseSect += "## License\n";
-    licenseSect += "Please see " + renderLicenseLink(license) +  "to get detailed information for this license\n";
-  }
-
-  return licenseSect;
 }
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const sections = ["Description", "Install", "Usage", "Contributing", "Test", "License", "Questions"];
-
-  let markdown = "#" + data.title + "\n";
-
-  markdown += renderLicenseBadge(data.license) + "\n";
-
-  markdown += "## Table of Contents\n";
-  for (let i=0; i<sections.length; i++) {
-    if (! (sections[i] === "License" && data.license === "None")) {
-      markdown += `${i + 1}.[${sections[i]}](#${sections[i][0].toLowerCase()}${sections[i].substring(1)})\n`;
-    }
-  }
-  markdown += "\n";
-
-  markdown += "##" + sections[0] + "\n";
-  markdown += data.description + "\n";
-
-  markdown += "##" + sections[0] + "\n";
-  markdown += data.installation + "\n";
-
-  markdown += "##" + sections[0] + "\n";
-  markdown += data.usage + "\n";
-
-  markdown += "##" + sections[0] + "\n";
-  markdown += data.contribute + "\n";
-  
-  markdown += "##" + sections[0] + "\n";
-  markdown += data.test + "\n";
-  
- markdown += renderLicenseSection(data.license) + "\n";
-
- markdown += "##" + sections[6] + "\n";
- markdown += "You can find me [HERE](https://github.com/" + data.username + ") on Github\n";
- markdown += "Please contact me via email at " + data.email + "for more information or if you have additional questions.\n";
-
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  *[Usage](#usage)
+  ${data.license !== 'None' ? '*[License(#license)' : ''}
+  *[Questions](#questions)
+
+  ##Installation
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  node index.js
+  \`\`\`
+
+  ${renderLicenseSection(data.license)}
+
+  ##Questions
+  For questions about this project, please reach out to me on GitHub: [@${data.github}](https://github.com/${data.github})
 `;
 }
 
