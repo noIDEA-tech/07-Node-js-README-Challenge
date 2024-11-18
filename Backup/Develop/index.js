86c4537de6147a4d1b75df76fd7aaa40a2029daf
Nancy Watreas <nwatreas2023@gmail.com>
2024-11-17 21:14:04 -0600

    added modified index file

--- Backup/Develop/index.js
+++ Backup/Develop/index.js
@@ -9,42 +9,37 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
-        validate: (input) => input.length > 0 ? true : 'Title is required!'
     },
     {
         type: 'input',
         name: 'description',
         message: 'Enter a description of your project:',
-        validate: (input) => input.length > 0 ? true : 'Description is required!'
     },
     {
         type: 'list',
         name: 'license',
         message: 'Choose a license for this project:',
-        choices: ['MIT', 'Apache 2.0', 'BSD 3-Clause', 'None']
+        choices: ['MIT', 'Apache 2.0', 'BSD', 'None']
     },
     {
         type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions:',
-        default: 'npm install'
+        name: 'install',
+        message: 'Enter the installation instructions:',
     },
     {
         type: 'input',
         name: 'usage',
         message: 'Please enter information on how to use this project:',
-        validate: (input) => input.length > 0 ? true : 'Usage information required!'
     },
     {
         type: 'input',
-        name: 'contribute',
+        name: 'guidelines',
         message: 'Please enter contribution guidelines:',
     },
     {
         type: 'input',
-        name: 'test',
+        name: 'tests',
         message: 'Please enter test instructions:',
-        validate: (input) => input.length > 0 ? true : 'Test instructions required!'
     },
     {
         type: 'input',
@@ -62,19 +57,22 @@ const questions = [
 async function writeToFile(fileName, data) {
     try {
         await fs.writeFile(fileName, data);
-        console.log('Succesfully created README.md!');
+        console.log('Successfully created README.md!');
     } catch (err) {
         console.error('Error writing file:', err);
     }
 }
- 
+
+// Function to initialize app
 async function init() {
     try {
-
+        // Get user inputs
         const answers = await inquirer.prompt(questions);
-
+        
+        // Generate markdown content
         const markdownContent = generateMarkdown(answers);
-
+        
+        // Write to README.md file
         await writeToFile('README.md', markdownContent);
     } catch (err) {
         console.error('Error initializing app:', err);
@@ -83,5 +81,3 @@ async function init() {
 
 init();
 
-//MOVED COMMENTS TO LINE 89 TODO: Create a function to initialize app//
-//MOVED COMMENTS TO LINE 90//
